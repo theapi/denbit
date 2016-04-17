ab285f87b7f3e9428a15d090d55975da668502aa
theapi <petermclarke@gmail.com>
2016-04-17 17:52:29 +0100

    Internal pullup on the switches

--- arduino/SmokeTest/SmokeTest.ino
+++ arduino/SmokeTest/SmokeTest.ino
@@ -1,21 +1,29 @@
 
-#define LED_S_GREEN   12 // D6
-#define LED_S_RED     13 // D7
+#define LED_S_GREEN   13 // D7
+#define LED_S_RED     12 // D6
 #define LED_RGB_RED   15 // D8
 #define LED_RGB_GREEN 10 // SD3
 #define LED_RGB_BLUE  14 // D5
-
+#define SW1 2
+#define SW2 0
 
 void setup() {
+
   pinMode(LED_S_GREEN, OUTPUT);
   pinMode(LED_S_RED, OUTPUT);
 
+// RGB led
   pinMode(LED_RGB_RED, OUTPUT);
   pinMode(LED_RGB_GREEN, OUTPUT);
   pinMode(LED_RGB_BLUE, OUTPUT);
+
+// Switches
+  pinMode(SW1, INPUT_PULLUP);
+  pinMode(SW2, INPUT_PULLUP);
 }
 
 void loop() {
+
   digitalWrite(LED_S_GREEN, HIGH);
   digitalWrite(LED_S_RED, HIGH);
 
@@ -25,10 +33,17 @@ void loop() {
   
   delay(1000);      
 
-  
-  digitalWrite(LED_S_GREEN, LOW);  
-  digitalWrite(LED_S_RED, LOW); 
+ byte sw1_state = digitalRead(SW1);
+ byte sw2_state = digitalRead(SW2);
 
+  // Do not flash if the button is not held down.
+  if (sw1_state) {
+    digitalWrite(LED_S_GREEN, LOW);  
+  }
+  if (sw2_state) {
+    digitalWrite(LED_S_RED, LOW); 
+  }
+  
   digitalWrite(LED_RGB_RED, LOW); 
   digitalWrite(LED_RGB_GREEN, LOW); 
   digitalWrite(LED_RGB_BLUE, LOW); 
