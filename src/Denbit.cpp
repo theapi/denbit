f7fb4577e9f77861c9ce938e62b4e847ac9c66f0
theapi <petermclarke@gmail.com>
2016-05-02 13:35:31 +0100

    Initialise the Denbit with the library

--- src/Denbit.cpp
+++ src/Denbit.cpp
@@ -1,4 +1,26 @@
-
+#include "Arduino.h"
 #include "Denbit.h"
 
+Denbit::Denbit()
+{
+  // Single LEDs
+  pinMode(DENBIT_GREEN, OUTPUT);
+  pinMode(DENBIT_RED, OUTPUT);
 
+  // RGB led
+  pinMode(DENBIT_RGB_RED, OUTPUT);
+  pinMode(DENBIT_RGB_GREEN, OUTPUT);
+  pinMode(DENBIT_RGB_BLUE, OUTPUT);
+
+  // Switches
+  pinMode(DENBIT_SW1, INPUT_PULLUP);
+  pinMode(DENBIT_SW2, INPUT_PULLUP);
+
+
+  digitalWrite(DENBIT_GREEN, LOW);
+  digitalWrite(DENBIT_RED, LOW);
+
+  digitalWrite(DENBIT_RGB_RED, LOW);
+  digitalWrite(DENBIT_RGB_GREEN, LOW);
+  digitalWrite(DENBIT_RGB_BLUE, LOW);
+}
--- src/Denbit.h
+++ src/Denbit.h
@@ -1,8 +1,22 @@
+#ifndef DENBIT_H
+#define DENBIT_H
 
-#ifndef DENBIT_H_
-#define DENBIT_H_
+#include "Arduino.h"
 
-#include <Arduino.h>
+#define DENBIT_GREEN     13 // D7
+#define DENBIT_RED       12 // D6
+#define DENBIT_RGB_RED   15 // D8
+#define DENBIT_RGB_GREEN 14 // D5
+#define DENBIT_RGB_BLUE  10 // SD3
+#define DENBIT_SW1 2
+#define DENBIT_SW2 16
 
+class Denbit
+{
+  public:
+    Denbit();
+  private:
+    int _mode;
+};
 
-#endif /* DENBIT_H_ */
+#endif
