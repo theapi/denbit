68ed03cd85d11dc9641fc5bbaab071db94aa6733
Peter Clarke <petermclarke@gmail.com>
2016-05-11 13:55:01 +0100

    Basic setup

--- examples/ServoWebsocket/ServoWebsocket.ino
+++ examples/ServoWebsocket/ServoWebsocket.ino
@@ -1,7 +1,8 @@
-
-/** 
-  Servo Sweep
-*/
+/*
+ * Servo WebSocketServer
+ *
+ * Install the library from https://github.com/Links2004/arduinoWebSockets
+ */
 
 #include <Arduino.h>
 
@@ -19,14 +20,7 @@
 // Initialize the denbit.
 Denbit denbit;
 
-Servo myservo;  // create servo object to control a servo
 
-// Create a wifi object.
-ESP8266WiFiMulti WiFiMulti;
-// Create a webserver object.
-ESP8266WebServer server = ESP8266WebServer(80);
-// Create the websocket server.
-WebSocketsServer webSocket = WebSocketsServer(81);
 
 // Assign readable names to the led pin numbers
 const int ledRed =  15; 
@@ -35,9 +29,13 @@ const int ledBlue =  10;
 
 int servoPosition; // Where to move the servo to.
 
-/**
- * Called when something happens on the websocket.
- */
+Servo servoA;  // create servo object to control a servo
+
+ESP8266WiFiMulti WiFiMulti;
+
+ESP8266WebServer server = ESP8266WebServer(80);
+WebSocketsServer webSocket = WebSocketsServer(81);
+
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t lenght) {
 
     switch(type) {
@@ -55,7 +53,7 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t lenght
         case WStype_TEXT:
             Serial.printf("[%u] get Text: %s\n", num, payload);
 
-            if (payload[0] == '#') {
+            if(payload[0] == '#') {
                 // we get RGB data
 
                 // decode rgb data
@@ -68,8 +66,7 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t lenght
                 // Move the servo to according to the red value.
                 int redVal = (rgb >> 16) & 0xFF;
                 servoPosition = map(redVal, 0, 255, 0, 180); // scale it to use it with the servo (value between 0 and 180)
-                myservo.write(servoPosition);             // sets the servo position according to the scaled value
-
+                servoA.write(servoPosition);             // sets the servo position according to the scaled value
             }
 
             break;
@@ -78,10 +75,11 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t lenght
 }
 
 void setup() {
+    //Serial.begin(921600);
     Serial.begin(115200);
-    myservo.attach(12);  // attaches the servo to the output pin
 
     Serial.setDebugOutput(true);
+
     Serial.println();
     Serial.println();
     Serial.println();
@@ -92,9 +90,39 @@ void setup() {
         delay(1000);
     }
 
-    digitalWrite(ledRed, 1);
-    digitalWrite(ledGreen, 1);
-    digitalWrite(ledBlue, 1);
+    digitalWrite(ledRed, 0);
+    digitalWrite(ledGreen, 0);
+    digitalWrite(ledBlue, 0);
+
+    WiFiMulti.addAP("SSID", "passpasspass");
+
+    while(WiFiMulti.run() != WL_CONNECTED) {
+        delay(100);
+    }
+
+    // start webSocket server
+    webSocket.begin();
+    webSocket.onEvent(webSocketEvent);
+
+    if(MDNS.begin("esp8266")) {
+        Serial.println("MDNS responder started");
+    }
+
+    // handle index
+    server.on("/", []() {
+        // send index.html
+        server.send(200, "text/html", "<html><head><script>var connection = new WebSocket('ws://'+location.hostname+':81/', ['arduino']);connection.onopen = function () {  connection.send('Connect ' + new Date()); }; connection.onerror = function (error) {    console.log('WebSocket Error ', error);};connection.onmessage = function (e) {  console.log('Server: ', e.data);};function sendRGB() {  var r = parseInt(document.getElementById('r').value).toString(16);  var g = parseInt(document.getElementById('g').value).toString(16);  var b = parseInt(document.getElementById('b').value).toString(16);  if(r.length < 2) { r = '0' + r; }   if(g.length < 2) { g = '0' + g; }   if(b.length < 2) { b = '0' + b; }   var rgb = '#'+r+g+b;    console.log('RGB: ' + rgb); connection.send(rgb); }</script></head><body>LED Control:<br/><br/>R: <input id=\"r\" type=\"range\" min=\"0\" max=\"255\" step=\"1\" onchange=\"sendRGB();\" /><br/>G: <input id=\"g\" type=\"range\" min=\"0\" max=\"255\" step=\"1\" onchange=\"sendRGB();\" /><br/>B: <input id=\"b\" type=\"range\" min=\"0\" max=\"255\" step=\"1\" onchange=\"sendRGB();\" /><br/></body></html>");
+    });
+
+    server.begin();
+
+    // Add service to MDNS
+    MDNS.addService("http", "tcp", 80);
+    MDNS.addService("ws", "tcp", 81);
+
+    digitalWrite(ledRed, 0);
+    digitalWrite(ledGreen, 0);
+    digitalWrite(ledBlue, 0);
 
 }
 
