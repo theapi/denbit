6ddcd60bcb65380838a20455f425d58130cf9c73
Peter Clarke <petermclarke@gmail.com>
2016-05-23 13:28:33 +0100

    Use the library

--- examples/WiFiAccessPoint/WiFiAccessPoint.ino
+++ examples/WiFiAccessPoint/WiFiAccessPoint.ino
@@ -1,50 +1,25 @@
-/*
- * Copyright (c) 2015, Majenko Technologies
- * All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- * 
- * * Redistributions of source code must retain the above copyright notice, this
- *   list of conditions and the following disclaimer.
- * 
- * * Redistributions in binary form must reproduce the above copyright notice, this
- *   list of conditions and the following disclaimer in the documentation and/or
- *   other materials provided with the distribution.
- * 
- * * Neither the name of Majenko Technologies nor the names of its
- *   contributors may be used to endorse or promote products derived from
- *   this software without specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 
-/* Create a WiFi access point and provide a web server on it. */
+/**
+ * Create a WiFi access point and provide a web server on it. 
+ */
 
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h> 
 #include <ESP8266WebServer.h>
 
-#define LED_S_GREEN   13 // D7
-#define LED_S_RED     12 // D6
-#define LED_RGB_RED   15 // D8
-#define LED_RGB_GREEN 14 // D5
-#define LED_RGB_BLUE  10 // SD3
-#define SW1 2
-#define SW2 16
+// Include the Denbit library.
+#include <Denbit.h>
+// Initialize the denbit.
+Denbit denbit;
+
+// Assign readable names to the led pin numbers
+const int ledRgbRed   =  15; 
+const int ledRgbGreen =  14; 
+const int ledRgbBlue  =  10; 
 
 /* Set these to your desired credentials. */
 const char *ssid = "ESPap";
-const char *password = "123";
+const char *password = "12345678";
 
 ESP8266WebServer server(80);
 
@@ -56,40 +31,28 @@ void handleRoot() {
 }
 
 void handleLed() {
-  if (digitalRead(LED_RGB_GREEN)) {
-    digitalWrite(LED_RGB_RED, LOW); 
-    digitalWrite(LED_RGB_GREEN, LOW); 
-    digitalWrite(LED_RGB_BLUE, LOW); 
+  if (digitalRead(ledRgbGreen)) {
+    digitalWrite(ledRgbRed, LOW); 
+    digitalWrite(ledRgbGreen, LOW); 
+    digitalWrite(ledRgbBlue, LOW); 
     server.send(200, "text/html", "<h1>LED OFF</h1><h2><a href=\"/led\">reload</a></h2>");
   } else {
-    digitalWrite(LED_RGB_RED, HIGH); 
-    digitalWrite(LED_RGB_GREEN, HIGH); 
-    digitalWrite(LED_RGB_BLUE, HIGH); 
+    digitalWrite(ledRgbRed, HIGH); 
+    digitalWrite(ledRgbGreen, HIGH); 
+    digitalWrite(ledRgbBlue, HIGH); 
     server.send(200, "text/html", "<h1>LED ON</h1><h2><a href=\"/led\">reload</a></h2>");
   }
   
 }
 
 void setup() {
-
-  pinMode(LED_S_GREEN, OUTPUT);
-  pinMode(LED_S_RED, OUTPUT);
-
-  // RGB led
-  pinMode(LED_RGB_RED, OUTPUT);
-  pinMode(LED_RGB_GREEN, OUTPUT);
-  pinMode(LED_RGB_BLUE, OUTPUT);
-
-  // Switches
-  pinMode(SW1, INPUT_PULLUP);
-  pinMode(SW2, INPUT_PULLUP);
   
-	delay(1000);
 	Serial.begin(115200);
 	Serial.println();
 	Serial.print("Configuring access point...");
 	/* You can remove the password parameter if you want the AP to be open. */
 	//WiFi.softAP(ssid, password);
+  WiFi.softAP(ssid);
 
 	IPAddress myIP = WiFi.softAPIP();
  
@@ -101,11 +64,6 @@ void setup() {
  
 	Serial.println("HTTP server started");
   
-  digitalWrite(LED_RGB_RED, LOW); 
-  digitalWrite(LED_RGB_GREEN, LOW); 
-  digitalWrite(LED_RGB_BLUE, LOW); 
-  digitalWrite(LED_S_GREEN, HIGH);
-  digitalWrite(LED_S_RED, LOW);
 }
 
 void loop() {
