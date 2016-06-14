87a61985d8182b6f5206607e33870da6cda56c8e
theapi <petermclarke@gmail.com>
2016-06-14 20:24:55 +0100

    Knob example

---
+++ examples/Basics/Knob/Knob.ino
@@ -0,0 +1,41 @@
+
+/** 
+   Knob (potentiometer)
+
+   This sketch reads value of the knob (potentiometer)
+   and sends the value to the serial output.
+
+   View the data in either the serial monitor or the serial plotter.
+
+   The circuit:
+	 - connect the middle pin of the potentiometer to the analog pin A0
+	 - connect one of the outer pins of the potentiometer to the 3V3 (power)
+	 - connect the other outer pin of the potentiometer to the GND (ground)
+
+*/
+
+// Include the Denbit library.
+#include <Denbit.h>
+// Initialize the denbit.
+Denbit denbit;
+
+// these constants won't change:
+const int pot = A0; // the potentiometer is connected to analog pin 0
+
+// these variables will change:
+int potValue = 0;      // variable to store the value read from the sensor pin
+
+
+void setup() {
+  // Inital the serial port to output sensor values.
+  Serial.begin(115200);  
+}
+
+void loop() {
+  // read the sensor and store it in the variable sensorReading:
+  potValue = analogRead(pot);
+  Serial.println(potValue);
+  
+  delay(100);  // delay to avoid overloading the serial port buffer
+}
+
