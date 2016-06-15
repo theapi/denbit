bc5d6dba1fa80b6a07847659e0de13877184eca4
Peter Clarke <petermclarke@gmail.com>
2016-06-15 13:50:10 +0100

    Images & links

--- examples/Basics/AnalogReadSerial/AnalogReadSerial.ino
+++ examples/Basics/AnalogReadSerial/AnalogReadSerial.ino
@@ -2,16 +2,17 @@
   AnalogReadSerial
   Reads an analog input on pin 0, prints the result to the serial monitor.
 
+  Connect a wire to the connection marked A0.
+  An image & more info on the setup is at https://github.com/theapi/denbit/blob/master/docs/examples/Basics/AnalogReadSerial.md
+
   To see the serial monitor, click the magnifiying class icon in the top right.
   The serial monitor needs to be listening at "115200 baud", 
   so set the value in the bottom left to 115200 baud.
   If the wrong value is set, only gibberish will appear on the monitor.
   
   Graphical representation is available using serial plotter (Tools > Serial Plotter menu)
-  Attach the center pin of a potentiometer to pin A0, 
-  and the outside pins to 3V3 and GND.
 
-*/
+  */
 
 /**
  * The setup function runs once when you press reset.
