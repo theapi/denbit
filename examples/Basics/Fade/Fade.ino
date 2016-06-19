4c916248b5bbefd386d5859e780191c07b8e0ebf
Peter Clarke <petermclarke@gmail.com>
2016-06-19 12:43:56 +0100

    Update Fade.ino

--- examples/Basics/Fade/Fade.ino
+++ examples/Basics/Fade/Fade.ino
@@ -4,12 +4,6 @@
  This example shows how to fade the Red LED on pin 12 
  using the analogWrite() function.
 
- The analogWrite() function uses PWM, so if
- you want to change the pin you're using, be
- sure to use another PWM capable pin. On most
- Arduino, the PWM pins are identified with 
- a "~" sign, like ~3, ~5, ~6, ~9, ~10 and ~11.
-
  */
 
 int led = 12;           // the PWM pin the LED is attached to
