1d39baab64d08d6165ad474224cd121f27afcfa7
Peter Clarke <petermclarke@gmail.com>
2016-07-25 11:33:05 +0100

    MQTT

---
+++ examples/MQTT/MQTT.ino
@@ -0,0 +1,137 @@
+/*
+ Basic ESP8266 MQTT example
+
+ This sketch demonstrates the capabilities of the pubsub library in combination
+ with the ESP8266 board/library.
+
+ It connects to an MQTT server then:
+  - publishes "hello world" to the topic "outTopic" every two seconds
+  - subscribes to the topic "inTopic", printing out any messages
+    it receives. NB - it assumes the received payloads are strings not binary
+  - If the first character of the topic "inTopic" is an 1, switch ON the ESP Led,
+    else switch it off
+
+ It will reconnect to the server if the connection is lost using a blocking
+ reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
+ achieve the same result without blocking the main loop.
+
+ To install the PubSubClient:
+   In the Arduino IDE Sketch -> Include Library -> Manage Libraries
+   search for "pubsub" and install.
+
+ To install the MQTT broker & clients on Ubununtu / Raspberry Pi
+sudo apt-get install mosquitto, mosquitto-clients
+
+ To test the installaiton:
+mosquitto_sub -h <mqtt_server> -p 1883 -i foo -t outTopic
+mosquitto_pub -h <mqtt_server> -p 1883 -i bar -t inTopic -m '1'
+
+*/
+
+#include <ESP8266WiFi.h>
+// From the library manager
+#include <PubSubClient.h>
+
+#include <Denbit.h>
+// Initialize the denbit.
+Denbit denbit;
+
+
+// Update these with values suitable for your network.
+const char* ssid = "******";
+const char* password = "******";
+const char* mqtt_server = "******";
+uint16_t mqtt_port = 1883;
+
+const int indicatorLed = 13;    // the green LED will indicate when a reading is being made
+
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
+void setup() {
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+}
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    digitalWrite(indicatorLed, HIGH);   // Turn the LED on 
+  } else {
+    digitalWrite(indicatorLed, LOW);  // Turn the LED off
+  }
+
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP8266Client")) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("outTopic", "hello world");
+      // ... and resubscribe
+      client.subscribe("inTopic");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+void loop() {
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  long now = millis();
+  if (now - lastMsg > 2000) {
+    lastMsg = now;
+    ++value;
+    snprintf (msg, 75, "hello world #%ld", value);
+    Serial.print("Publish message: ");
+    Serial.println(msg);
+    client.publish("outTopic", msg);
+  }
+}
